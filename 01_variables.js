fd9eac9ccdddfd8e6380042d4dc93fe4b46ba641
Moh Junaid <53346421+techjunaid@users.noreply.github.com>
2024-04-27 15:39:37 +0000

    basic codes

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+console.log ("Junaid Chaudhary");
+let a = 30
+let b = 40
+
+let c = a+b
+console.log(c)
---
+++ 02_basicPrograms.js
@@ -0,0 +1,27 @@
+// add two numbers
+
+let num1 = 67
+let num2 = 56
+
+let sum = num1 + num2
+//console.log("sum :", sum)
+
+// adding two numbers via functin
+
+function addingTwoNumbers (a,b){
+      
+    return a+b;
+}
+
+let num3 = 60
+let num4 = 50
+
+let sum1 = addingTwoNumbers (num1, num2)
+//console.log("sum of given number is:", sum1)
+
+// using arrow function
+
+let addition = (num1, num2) => (num1+num2)
+
+sum = addition(num1,num2)
+console.log(sum)
